f0d341ca15c492ae9f99b3a39e7032597a117d1c
Mat <MaT-PT@users.noreply.github.com>
2012-10-07 00:17:23 +0000

    * Google Analytics stuff.
    git-svn-id: svn://localhost/home/mat/projets/pokemon-signature-editor-web/pokemon-signature-editor-web_svn/trunk@27 3118c96e-77ff-5d48-becd-44e2f69b0a81

--- index.php
+++ index.php
@@ -15,7 +15,13 @@
 <script type="text/javascript" src="js/savefile.js"></script>
 <script type="text/javascript" src="js/sign.js"></script>
 <script type="text/javascript" src="js/details.js"></script>
-<?php if (!$IS_DEBUG) { ?><script type="text/javascript">window.onload=function(){var i=new Image();i.src='//affiliates.mozilla.org/link/banner/19565';i.src='//affiliates.mozilla.org/link/banner/20350'}</script><?php } ?>
+<?php if (!$IS_DEBUG) { ?>
+<script type="text/javascript">window.onload=function(){var i=new Image();i.src='//affiliates.mozilla.org/link/banner/19565';i.src='//affiliates.mozilla.org/link/banner/20350'}</script>
+<script type="text/javascript">var _gaq=_gaq||[];_gaq.push(['_setAccount','UA-23228708-8']);
+_gaq.push(['_trackPageview']);(function(){var ga=document.createElement('script');ga.type='text/javascript';ga.async=true;
+ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';
+var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);})();</script>
+<?php } ?>
 </head>
 <?php ob_flush(); flush(); ?>
 <body>
